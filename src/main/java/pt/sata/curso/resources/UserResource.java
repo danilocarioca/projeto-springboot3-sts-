d31234137f9b3d177f9fd929513991005bd0ed81
Danilo Carioca <danilocarioca83@gmail.com>
2023-02-26 02:31:44 -0100

    User entity and resource

---
+++ src/main/java/pt/sata/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package pt.sata.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pt.sata.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com" , "918273645", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
