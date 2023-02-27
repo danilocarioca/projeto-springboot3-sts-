7b416bf183ec2b629ac13c0f517d83772d0aeac8
Danilo <2021108315@uac.pt>
2023-02-27 17:28:24 -0100

    Exception handling - findById part 1

---
+++ src/main/java/pt/sata/curso/resources/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package pt.sata.curso.resources.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found, Id" + id );
+    }
+
+}
