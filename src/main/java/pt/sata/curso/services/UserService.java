650dc297e5d714ba6258371782c6d288f68689ce
Danilo Carioca <danilocarioca83@gmail.com>
2023-02-26 23:11:25 -0100

    Service layer, component registration

---
+++ src/main/java/pt/sata/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package pt.sata.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pt.sata.curso.entities.User;
+import pt.sata.curso.repositories.UserRepository;
+
+@Service
+public class UserService  {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();	}
+}
