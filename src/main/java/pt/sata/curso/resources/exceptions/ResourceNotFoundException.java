ae74f6a55704e16e95ef757740f718ccb1a08b7c
Danilo <2021108315@uac.pt>
2023-02-28 09:56:52 -0100

    Exception handling - findById part 2

--- src/main/java/pt/sata/curso/resources/exceptions/ResourceNotFoundException.java
+++ src/main/java/pt/sata/curso/resources/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package pt.sata.curso.resources.exceptions;
 public class ResourceNotFoundException extends RuntimeException{
 
     public ResourceNotFoundException(Object id) {
+
         super("Resource not found, Id" + id );
     }
-
 }
--- src/main/java/pt/sata/curso/services/UserService.java
+++ src/main/java/pt/sata/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import pt.sata.curso.entities.User;
 import pt.sata.curso.repositories.UserRepository;
+import pt.sata.curso.resources.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService  {
@@ -21,7 +22,7 @@ public class UserService  {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
